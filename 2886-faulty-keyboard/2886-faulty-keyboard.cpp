3373a5c6ac903d885706b0c016d7a31647891030
Sharad Jha <91933359+sharadj34@users.noreply.github.com>
2024-07-18 10:26:11 +0530

    Time: 4 ms (71.8%), Space: 9.9 MB (24.03%) - LeetHub

--- 2886-faulty-keyboard/2886-faulty-keyboard.cpp
+++ 2886-faulty-keyboard/2886-faulty-keyboard.cpp
@@ -1,34 +1,16 @@
 class Solution {
 public:
-    string reverseAns(string st){
-        int start = 0;
-        int end = st.size() - 1;
-
-        while(start < end)
-        {
-            swap(st[start], st[end]);
-            start++;
-            end--;
-        }
-
-        return st;
-    }
     string finalString(string s) {
         int n = s.size();
-        string ans = "";
-        for(int i=0; i<n; i++)
+        string res = "";
+        for (int i=0; i<n; i++)
         {
-            char ch = s[i];
-            if(ch == 'i')
-            {
-                ans = reverseAns(ans);
-            }
-
+            if (s[i] == 'i')
+                reverse(res.begin(), res.end());
             else 
-            {
-                ans = ans + ch;
-            }
+                res += s[i];
         }
-        return ans;
+
+        return res;
     }
 };
