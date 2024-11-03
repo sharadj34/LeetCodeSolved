04004bbc9d6d352024ef8de8385b1a91ab84d06d
Sharad Jha <91933359+sharadj34@users.noreply.github.com>
2024-11-03 08:47:25 +0530

    Time: 0 ms (100%), Space: 7.6 MB (55.81%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.cpp
+++ 0812-rotate-string/0812-rotate-string.cpp
@@ -1,37 +1,8 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-
-        // OPTIMAL APPROACH - O(n)
-        queue<char> q1,q2;
-
-        for(int i=0; i<s.size(); i++)
-            q1.push(s[i]);
-        
-        for(int i=0; i<goal.size(); i++)
-            q2.push(goal[i]);
-
-        int k = goal.size() - 1;
-
-        while(k!=0)
-        {
-            if(q1==q2)
-                return true;
-
-            char ch = q2.front();
-            q2.pop();
-            q2.push(ch);
-            k--;
-        }
-        return false;
-
-        // BRUTE FORCE APPROACH - O(n^2)
-        /*
-        if (s.size() != goal.size()) {
-            return false;
-        }
-        string concatenated = goal + goal;
-        return concatenated.find(s) != string::npos;
-        */
+        if (s.length() != goal.length()) return false;
+        string doubled = s + s;
+        return doubled.find(goal) != string::npos;
     }
 };
